038b1468e5b2703ecfe77ee87d3d4a5e16472eb2
מוני ברוך <monybaruch@gmail.com>
2023-03-12 16:26:29 +0200

    adding a BMI calculator

--- calculator.js
+++ calculator.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true })); // urlencoded is used to parse a data from a html form
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World</h1>");
+  res.sendFile(__dirname + "/calculator.html");
+});
+app.post("/", function (req, res) {
+  let number1 = Number(req.body.number1);
+  let number2 = Number(req.body.number2);
+  let result = number1 + number2;
+  res.send("The result of the calculation is: " + result);
+});
+app.get("/bmi", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+app.post("/bmi", function (req, res) {
+  let weight = parseFloat(req.body.weight);
+  let height = parseFloat(req.body.height);
+  let bmi = weight / (height * height);
+  res.send("The result of your BMI is: " + bmi);
 });
 
 app.listen(3000, function () {
